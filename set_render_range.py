892219a3634d379b732ee51a8501eb9337353361
Kieran Hanrahan <kieran@knhn.me>
2025-03-11 00:10:31 -0400

    Remove unnecessary blank lines

--- set_render_range.py
+++ set_render_range.py
@@ -39,13 +39,11 @@ MESSAGE_PREFIX = '[PYTHON]'
 
 def message(string):
     """Print message to shell window and append global MESSAGE_PREFIX."""
-
     print(' '.join([MESSAGE_PREFIX, string]))
 
 
 def set_to_current_frame(selection):
     """Set From & To values of the Render Range to be the current frame."""
-
     message(TITLE_VERSION)
     message('Script called from {}'.format(__file__))
 
@@ -61,7 +59,6 @@ def set_to_current_frame(selection):
 
 def scope_output_node(selection):
     """Filter for only Render or Write File nodes."""
-
     for node in selection:
         print(node.type.get_value())
         if not node.type.get_value() in ('Render', 'Write File'):
@@ -71,7 +68,6 @@ def scope_output_node(selection):
 
 def get_batch_custom_ui_actions():
     """Python hook to add custom right click menu item."""
-
     return [{'name': 'Edit...',
              'actions': [{'name': 'Set Render Range to Current Frame',
                           'isVisible': scope_output_node,
