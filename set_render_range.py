4b0154046eaa1d9c1c2676adaed915a617282b28
Kieran Hanrahan <kieran@knhn.me>
2025-03-11 00:20:03 -0400

    Bump version

--- set_render_range.py
+++ set_render_range.py
@@ -2,7 +2,7 @@
 Script Name: Set Render Range
 Written By: Kieran Hanrahan
 
-Script Version: 1.0.0
+Script Version: 2.0.0
 Flame Version: 2022
 
 URL: http://github.com/khanrahan/set-render-range
@@ -31,7 +31,7 @@ To Install:
 import flame
 
 TITLE = 'Set Render Range'
-VERSION_INFO = (1, 0, 0)
+VERSION_INFO = (2, 0, 0)
 VERSION = '.'.join([str(num) for num in VERSION_INFO])
 TITLE_VERSION = f'{TITLE} v{VERSION}'
 MESSAGE_PREFIX = '[PYTHON]'
